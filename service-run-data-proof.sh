aacdce0e842e958a2d91c9281a09839db5579bec
Isau AZEEZ <prodigy4442@gmail.com>
2021-01-29 12:46:08 +0100

    Fix script

--- service-run-data-proof.sh
+++ service-run-data-proof.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-java  -jar data-proof/target/data-proof-1.0-SNAPSHOT.jar server config.yml
+export REDIS_URL=localhost:6379
+export KAFKA_URL=localhost:9092
+export PRODUCER_TOPIC=input-data
+export CONSUMER_TOPIC=outout-data
+
+java  -jar data-proof/target/data-proof-1.0-SNAPSHOT.jar server data-proof/config.yml
