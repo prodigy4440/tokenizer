14876ce16c6ea8232b2239bcabdb2ac4bc092d68
Isau AZEEZ <prodigy4442@gmail.com>
2021-02-01 09:14:04 +0100

    update data source script

--- service-run-data-source.sh
+++ service-run-data-source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-KAFKA_URL
-PRODUCER_TOPIC
+export KAFKA_URL=localhost:9092
+export PRODUCER_TOPIC=card-info-topic
 java  -jar data-source/target/data-source-1.0-SNAPSHOT.jar server data-source/config.yml
